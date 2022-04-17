f332f8304a3db9a1d03b90effe50fa2502e7b4e6
Sai Rithwik M <rithwiksaim@gmail.com>
2022-04-17 19:21:19 +0530

    Updated ansible github actions setup

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-
-echo "Ansible Entrypoint"
-
-echo "This is the secret: $SSH_PASSWORD"
-
-echo "ansible_ssh_pass=$SSH_PASSWORD" >> /hosts
-echo "ansible_become_pass=$SSH_PASSWORD" >> /hosts
-
+echo "ansible_ssh_pass=$SSH_PASS" >> /hosts
+echo "ansible_become_pass=$SSH_PASS" >> /hosts
+echo "Performing Ansible commands"
 ansible-playbook ansible/playbook.yml --user rithwik
